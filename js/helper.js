58cfbae1ce89b29f0144a0104a1413c82db2fad7
snoop007nj <duonnguyen@yahoo.com>
2016-01-24 16:35:04 -0500

    add infoWindow to googlemaps

--- js/helper.js
+++ js/helper.js
@@ -181,6 +181,7 @@ function initializeMap() {
 
     // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
+      infoWindow.open(map, marker);
       // your code goes here!
     });
 
